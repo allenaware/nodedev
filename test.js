a2949e829c33e502ae8cffbf7f9d1403dd67ea73
allenaware <allenaware@163.com>
2015-06-22 22:33:47 +0800

    first

---
+++ test.js
@@ -0,0 +1,13 @@
+var redis = require("redis");
+var http = require('http');
+var url = require("url");
+client = redis.createClient(6379,'127.0.0.1',{});
+client.auth("allenpasswd", function(){});
+client.on("error", function (err) {
+    console.log("Error " + err);
+});
+http.createServer(function (req, res) {
+    var params = url.parse(req.url, true).query;
+   //validate the parameters
+    client.hset("win", params.guid,params.value,redis.print);
+}).listen(8888);
